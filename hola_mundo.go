f66fd6b167830a969eb818a1b36383ec1d6f54c2
SolidDogor <olivosralejandromatias@gmail.com>
2025-06-27 22:27:21 -0500

    Hola mundo: Comentarios y variables

--- hola_mundo.go
+++ hola_mundo.go
@@ -3,5 +3,38 @@ package main
 import "fmt"
 
 func main() {
-    fmt.Println("Hola Mundo!")
+
+	//Comentario de una linea
+
+	/*
+	   Comentario
+	   de
+	   varias
+	   lineas
+	*/
+
+	fmt.Println("Hola Mundo!")
+
+	//Variables
+	//Declaraación general: var nombre tipo
+
+	var myString string = "Esto es una cadena de texto"
+	fmt.Println(myString)
+
+	//Variando una variable
+	myString = "Y esta tambien es una cadena de texto!"
+	fmt.Println(myString)
+
+	//Se puede declarar sin especificar el tipo si se inicializa en la misma linea
+	var myString2 = "Esta es otra cadena de texto"
+	fmt.Println(myString2)
+
+	//Trabajando con int (por defecto int = int32)
+	var myInt int = 7
+	fmt.Println(myInt)
+	myInt = myInt + 4
+	fmt.Println(myInt)
+	fmt.Println(myInt - 1) //El valor de myInt no cambia tras esto
+	fmt.Println(myInt)
+
 }
