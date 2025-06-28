19014e032e273949148fc71615e814809da574b2
SolidDogor <olivosralejandromatias@gmail.com>
2025-06-27 22:52:40 -0500

    Hola mundo: Más variables y control de flujo

--- hola_mundo.go
+++ hola_mundo.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"reflect"
+)
 
 func main() {
 
@@ -36,5 +39,44 @@ func main() {
 	fmt.Println(myInt)
 	fmt.Println(myInt - 1) //El valor de myInt no cambia tras esto
 	fmt.Println(myInt)
+
+	myString2 = "Mostrando un entero con una cadena "
+	fmt.Println(myString2, myInt)
+
+	//Mostrando el tipo de dato con la biblioteca reflect
+	var myType = reflect.TypeOf(myInt)
+	fmt.Println(myType)
+
+	//Trabajando con flotantes
+	var myFloat float64 = 6.5
+	fmt.Println(myFloat)
+	fmt.Println(reflect.TypeOf(myFloat))
+
+	//Sumando un int convertido a float con otro float
+	fmt.Println(float64(myInt) + myFloat)
+
+	//Trabajando con booleanos
+	var myBool bool = false
+	myBool = true
+	fmt.Println(myBool)
+
+	//Variable declarada e inicializada de manera abreviada
+	myString3 := "Esto es ootra cadena de texto"
+	fmt.Println(myString3)
+
+	//Trabajando con constantes
+	const myConst = "Esto es una constante"
+	fmt.Println(myConst)
+
+	//Control de flujo
+
+	myString = "Hola"
+	if myInt == 10 && myString == "Hola" {
+		fmt.Println("El valor es 10")
+	} else if myInt == 11 || myString == "Hola" {
+		fmt.Println("El valor es 11")
+	} else {
+		fmt.Println("El valor no es 10")
+	}
 
 }
