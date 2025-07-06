8e0173db48a12f44cf5129af711ef21e7d6642f6
SolidDogor <olivosralejandromatias@gmail.com>
2025-07-05 19:25:36 -0500

    Creando dos variables. Una copia el valor, otra apunta a la dirección de memoria

--- Punteros/Punteros.go
+++ Punteros/Punteros.go
@@ -11,4 +11,12 @@ func Punteros() {
 
 	fmt.Println(nombre1, &nombre1)
 	fmt.Println(nombre2, &nombre2)
+	//Creamos una nueva variable que tendrá el mismo valor que nombre1
+	//ocupamos 2 espacios de memoria con el mismo valor
+	nombre3 := nombre1
+	fmt.Println(nombre3, &nombre3)
+	//Si queremos utilizar el mismo valor que nombre1 podemos
+	//podemos utilizar un puntero que apunte a la dirección de memoria que nombre1
+	nombre4 := &nombre1
+	fmt.Println(nombre4, *nombre4)
 }
